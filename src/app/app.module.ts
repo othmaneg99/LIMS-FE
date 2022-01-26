f89ab2eefabcbee2d6855bbe2adefc814b986763
Ilyas <ilmakroum@gmail.com>
2022-01-26 23:11:16 +0100

    Setup internationalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticlesCatalogueComponent } from './articlesCatalogue/articlesCatalogue.component';
 import { RouterModule } from '@angular/router';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { ArticlesComponent } from './articles/articles.component';
 import { HomeComponent } from './home/home.component';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -18,14 +23,20 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'fr',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (HttpLoaderFactory),
+        deps: [HttpClient]
+      }
+    }),
     AppRoutingModule,
     RouterModule.forRoot([
       {path: 'articles', component: ArticlesCatalogueComponent},
       {path: 'articles', component: ArticlesComponent},
-
-
     ]),
-    HttpClientModule
 
   ],
   providers: [],
